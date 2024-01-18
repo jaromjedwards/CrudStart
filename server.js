b4469c8dcfdfad962c59e147a449b0c99464e1d7
Jarom Edwards <jaromjedwards@gmail.com>
2024-01-17 21:18:14 -0700

    everything but delete worked

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ var user = {
 
 app.get('/listUsers', function (req, res) {
     fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       console.log( data );
-       res.end( data );
+       console.log(data);
+       res.end(data);
     });
  })
 
@@ -25,7 +25,28 @@ app.get('/listUsers', function (req, res) {
        data = JSON.parse( data );
        data["user4"] = user["user4"];
        console.log( data );
-       res.end( JSON.stringify(data));
+       res.end(JSON.stringify(data));
+    });
+ })
+
+ app.get('/:id', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       var users = JSON.parse( data );
+       var user = users["user" + req.params.id] 
+       console.log( user );
+       res.end(JSON.stringify(user));
+    });
+ })
+
+ app.delete('/deleteUser', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       data = JSON.parse(data);
+       delete data["user2"];
+
+       console.log(data);
+       res.end(JSON.stringify(data));
     });
  })
 
